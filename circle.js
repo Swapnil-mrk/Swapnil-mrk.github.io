e87cb06bf966efa410deae8315526c63a331966a
Swapnil-mrk <108859618+Swapnil-mrk@users.noreply.github.com>
2022-07-07 01:23:33 -0700

    Add files via upload

---
+++ circle.js
@@ -0,0 +1,10 @@
+function calculate(){
+	var n1 = document.getElementById('txt1').value;
+	var area = (parseInt(n1) * 3.14 * parseInt(n1));
+	console.log(area);
+	document.getElementById('txt2').value = area;
+	var perimeter =(2*3.14*parseInt(n1));
+	console.log(perimeter);
+	document.getElementById('txt3').value = perimeter;
+		
+}
