3e9ded038f7409127fb8f41865c6d92a4f7bc9fe
Swapnil-mrk <108859618+Swapnil-mrk@users.noreply.github.com>
2022-07-07 01:03:15 -0700

    Delete compound1.js

--- compound1.js
+++
@@ -1,9 +0,0 @@
-function add(){
-	var n1 = document.getElementById('txt1').value;
-	var n2= document.getElementById('txt2').value;
-	var n3= document.getElementById('txt3').value;
-	var result=(parseInt(n1)*(1+(parseInt(n3)/100))^(parseInt(n2)));
-	console.log(result)
-	document.getElementById('txt4').value=result;
-	
-}
